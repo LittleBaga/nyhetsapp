3c095d4facc4c46d66aeb2c6227d8525e7ca2f76
Ilma <ilmce@hotmail.com>
2019-04-01 12:25:47 +0200

    test

--- src/components/Nyhetsartikel.js
+++ src/components/Nyhetsartikel.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 function Nyhetsartikel() {
   return (
+    <section>
     <article>
       <img src="https://source.unsplash.com/random/200×125/?cat" />
       <h2>Testnyhet</h2>
       <p>Beskrivning av testnyheten</p>
       <a>Läs mer..</a>
     </article>
+    </section>
   );
  }
 
